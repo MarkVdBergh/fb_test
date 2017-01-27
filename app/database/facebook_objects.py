6f1c54875dd6246d8cb28c82b7c43775c530b98f
vandenbergh.mark@gmail.com <MarkVdBergh>
2017-01-27 19:17:05 +0100

    Hola

--- app/database/facebook_objects.py
+++ app/database/facebook_objects.py
@@ -35,6 +35,8 @@ class FbRawPost_comments(DynamicEmbeddedDocument):
 
 class FbRawPosts(DynamicDocument):
     # ToDo: Extend the class with additional fields, like picture, name, message, ...
+    # ToDo: the field 'created_time_dt' is called 'created_time_local' in some documents !
+
     """
         Maps facebook Graph Api documents in a FbRawPosts object
         FbRawPosts implements following methods:
@@ -52,10 +54,9 @@ class FbRawPosts(DynamicDocument):
     reactions = EmbeddedDocumentListField(document_type=FbRawPost_reactions)
     comments = EmbeddedDocumentListField(document_type=FbRawPost_comments)
     shares = DictField()
-    message=StringField()
-    created_time_dt=DateTimeField()
-    created_time_local=DateTimeField()
-    pass
+    message = StringField()
+    created_time_dt = DateTimeField()
+    created_time_local = DateTimeField()
 
     def set(self):
         self.meta = 1
@@ -75,11 +76,16 @@ class FbRawPosts(DynamicDocument):
 @timecall()
 def test():
     x = FbRawPosts
-    for i in x.objects(shares__count__gte=10000):
-        print '='*100
+    # for i in x.objects(shares__count__gte=10000):
+    # This doesn't work ! see: https://docs.mongodb.com/manual/reference/operator/query/size/#_S_size. "create a counter field that you increment when you add elements to a field."
+    # for i in x.objects(comments__size__gte=100):
+    for i in x.objects(comments__1600__exists=True):
+        print '=' * 100
         print 'http://facebook.com/{}'.format(i.postid)
-        print('{}:   postid: {}, shares: {}'.format(i.profile.name, i.postid, i.shares['count']))
+        print('{}:   postid: {}, comments: {}'.format(i.profile.name,i.postid, len(i.comments)))
         print i.created_time_local, i.created_time_dt
         print i.message
-        print '='*100
+        print '=' * 100
+
+
 test()
---
+++ app/database/stats_objects.py
@@ -0,0 +1,6 @@
+
+
+
+# ToDo: ToDo: mongo $size can't query size of lists (see: https://docs.mongodb.com/manual/reference/operator/query/size/#_S_size)
+#      workaround: "create a counter field that you increment when you add elements to a field."
+# ToDo:
